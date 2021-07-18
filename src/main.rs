8d022708c475c2dc87d86e6484afa5591e3ea665
bimorphism <dhillonwilk@gmail.com>
2021-07-18 08:57:30 +0100

    added cli options to search videos and view their info

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,77 @@
 mod youtube;
+use std::io::Write;
+use std::error::Error;
+use clap::{AppSettings, Clap};
 
-fn main() {
-    //println!("Hello, world!");
+/// Command line interface for YouTube written in Rust
+#[derive(Clap)]
+#[clap(version = "1.0",author = "github.com/bimorphism/ytcli")]
+#[clap(setting = AppSettings::ColoredHelp)]
+struct Opts {
+    /// Search YouTube for a given query string
+    #[clap(short = 's', long = "search")]
+    search: Option<String>,
 
-    let results = youtube::search("mental outlaw", 2);
+    /// The amount of videos (the limit) that ytcli will show in the search results.
+    /// Defaults to 10.
+    #[clap(short = 'L', long = "limit", default_value = "10")]
+    limit: i32,
 
-    for res in results.unwrap() {
-        println!("ID: {}, Title: {}", res.id, res.title);
+    /// View information about a specific upload
+    #[clap(short = 'i', long = "info")]
+    info: Option<String>,
+}
+
+fn show_results(query: &str, limit: i32)
+     -> Result<(), Box<dyn Error>>
+{
+    let results = youtube::search(query, limit)?;
+
+    println!();
+    for (i, result) in results.iter().enumerate() {
+        println!("{}\tID: {}\t\tTitle: {}", i, result.id, result.title);
+        println!("-------------------------------------------");
     }
+
+    println!("Select a video to view (by its number)");
+    print!(">");
+    std::io::stdout().flush()?;
+
+
+    let mut line = String::new();
+    std::io::stdin().read_line(&mut line)?;
+
+    let line = line.strip_suffix('\n').unwrap();
+    let choice = line.parse::<usize>()?;
+    youtube::spawn_player(&results[choice], "mpv")?;
+
+    Ok(())
+}
+
+fn show_info(video_id: &str) -> Result<(), Box<dyn Error>> {
+    let result = youtube::search_id(video_id)?;
+
+    println!();
+    println!("-----------------------------------");
+    println!("Title: {}", result.title);
+    println!("ID: {}", result.id);
+    println!("Description: {}", result.description);
+    println!();
+    println!("Likes, dislikes: {}, {}", result.likes, result.dislikes);
+    
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let opts: Opts = Opts::parse();
+
+    if let Some(search) = opts.search {
+        show_results(&search, opts.limit)?;
+    }
+
+    if let Some(info) = opts.info {
+        show_info(&info)?;
+    }
+
+    Ok(())
 }
--- src/youtube.rs
+++ src/youtube.rs
@@ -3,19 +3,30 @@ use std::process::{Command, Stdio};
 use std::io::Read;
 
 use serde_json::Value;
+use spinners::{Spinner, Spinners};
 use std::result::Result;
 
 /// A Youtube search entry from youtube-dl
+#[derive(Clone)]
 pub struct YouTubeSearchEntry {
     pub id: String,
     pub description: String,
     pub title: String,
+    pub likes: i32,
+    pub dislikes: i32,
 }
 
+fn strip_quotes(s: String) -> String {
+    s.replace('"', "")
+}
+
+/// Searches YouTube for the given query string. The limit
+/// specifies how many results to include in the search, the higher the limit
+/// the more time it takes for youtube-dl to search however.
 pub fn search(query: &str, limit: i32) 
     -> Result<Vec<YouTubeSearchEntry>, Box<dyn Error>> 
 {
-    println!("Searching YouTube, please wait..");
+    let sp = Spinner::new(Spinners::Dots, "Searching YouTube, please wait..".into());
 
     let mut results: Vec<YouTubeSearchEntry> = Vec::new();
     let query = format!("ytsearch{}:{}", limit, query);
@@ -33,11 +44,41 @@ pub fn search(query: &str, limit: i32)
     let entries = json["entries"].as_array().unwrap();
     for entry in entries {
         results.push(YouTubeSearchEntry {
-            id: entry.get("id").unwrap().to_string(),
-            description: entry.get("description").unwrap().to_string(),
-            title: entry.get("title").unwrap().to_string(),
+            id: strip_quotes(entry.get("id").unwrap().to_string()),
+            description: strip_quotes(entry.get("description").unwrap().to_string()),
+            title: strip_quotes(entry.get("title").unwrap().to_string()),
+            likes: strip_quotes(entry.get("like_count").unwrap().to_string())
+                .parse::<i32>()?,
+            dislikes: strip_quotes(entry.get("dislike_count").unwrap().to_string())
+                .parse::<i32>()?,
         });
     }
 
+    sp.stop();
     Ok(results)
+}
+
+pub fn search_id(video_id: &str)
+    -> Result<YouTubeSearchEntry, Box<dyn Error>>
+{
+    let results = search(video_id, 1)?;
+    let result = &results[0];
+
+    Ok(result.clone())
+}
+
+/// Spawn a mpv/mplayer instance to handle the YouTube stream
+/// 
+/// NOTE: `entry` is a reference since otherwise the n'th item of the vector would be moved
+/// while the rest of the vector *isn't* moved and Rust screams at me :(
+pub fn spawn_player(entry: &YouTubeSearchEntry, player: &str) 
+    -> Result<(), Box<dyn Error>>
+{
+    println!("Opening https://youtube.com/watch?v={} in mpv", entry.id.replace('"', ""));
+
+    let _child = Command::new(player)
+        .arg(format!("https://youtube.com/watch?v={}", entry.id.replace('"', "")))
+        .spawn()?;
+
+    Ok(())
 }
